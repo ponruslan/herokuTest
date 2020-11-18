efe6b2bce36da7ec75f19da42f4bb017f26a563a
Ruslan Ponomarenko <panruslan@gmail.com>
2020-11-18 20:12:23 +0200

    first init

---
+++ src/main/java/com/example/testHeroku/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.example.testHeroku.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/")
+    public String main() {
+        return "hello world";
+    }
+}
---
+++ src/test/java/com/example/testHeroku/TestHerokuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testHeroku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestHerokuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
