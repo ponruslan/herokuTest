dbbea1b28e67ee139837c3f6436f53de63c8156b
Ruslan Ponomarenko <panruslan@gmail.com>
2020-12-02 18:54:22 +0200

    First release header
    - first change
    - third change

--- src/main/java/com/example/testHeroku/controller/MainController.java
+++ src/main/java/com/example/testHeroku/controller/MainController.java
@@ -1,13 +1,17 @@
 package com.example.testHeroku.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
 
+    @Value("${build.version}")
+    private String buildVersion;
+
     @GetMapping("/")
     public String main() {
-        return "hello world with memory limit";
+        return "BUILD VERSION: " + buildVersion;
     }
 }
