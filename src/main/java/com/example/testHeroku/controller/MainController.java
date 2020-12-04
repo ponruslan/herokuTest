59abae641612a85c0a8efa42b4d08e29694fbec3
Ruslan Ponomarenko <panruslan@gmail.com>
2020-12-04 09:22:31 +0200

    feat: setup BuildProperties

--- src/main/java/com/example/testHeroku/controller/MainController.java
+++ src/main/java/com/example/testHeroku/controller/MainController.java
@@ -1,17 +1,18 @@
 package com.example.testHeroku.controller;
 
-import org.springframework.beans.factory.annotation.Value;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class MainController {
 
-    @Value("${build.version}")
-    private String buildVersion;
+    private final BuildProperties buildProperties;
 
     @GetMapping("/")
     public String main() {
-        return "BUILD VERSION: " + buildVersion;
+        return "BUILD VERSION: " + buildProperties.getVersion();
     }
 }
